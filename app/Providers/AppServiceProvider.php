d560d778449419933e2ff11fd54e0bd23ce2754e
kidoyoshiyuki <kido@test>
2021-11-19 15:14:02 +0900

    modified AppServiceProvider App

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,6 @@ class AppServiceProvider extends ServiceProvider
             \URL::forseScheme('https');
             
         }
-        Schema::defaultStringLength(191);
+        //Schema::defaultStringLength(191);
     }
 }
